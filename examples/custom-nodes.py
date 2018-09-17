78f8b851f7a9a91cb58e90d2f1bf927b2aa79473
serderovsh <steein.shamsudin@gmail.com>
2018-09-17 06:32:19 +0300

    edited examples

--- examples/custom-nodes.py
+++ examples/custom-nodes.py
@@ -2,8 +2,6 @@ from tronapi.providers import HttpProvider
 from tronapi.tron import Tron
 
 # Recommend FullNode: http://13.125.210.234:8090
-
-
 fullNode = HttpProvider('https://api.trongrid.io:8090')
 
 # Example 1
--- examples/send-transaction.py
+++ examples/send-transaction.py
@@ -1,6 +1,7 @@
 from tronapi.tron import Tron
 
 tron = Tron('https://api.trongrid.io:8090')
+tron.private_key = 'private_key'
 
 result = tron.send_transaction('FromAddress', 'ToAddress', 1)
 
