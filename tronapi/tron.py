b18387134b88900b35b2a1ca5bbb032ab20d1819
serderovsh <steein.shamsudin@gmail.com>
2018-10-12 06:52:41 +0300

    code style

--- tronapi/tron.py
+++ tronapi/tron.py
@@ -1,21 +1,34 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# Copyright 2018-present, iEXBase, Inc.
-# All rights reserved.
+
+# Copyright 2018 iEXBase
 #
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-##############################################################################
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+    Tron API from Python
+"""
 
 import base58
 import math
 
 from tronapi.crypto import utils
-from tronapi.providers import HttpProvider
+from tronapi.provider import HttpProvider
 
 
 class Tron:
-    def __init__(self, full_node, solidity_node=None, event_server=None, private_key=None):
+
+    def __init__(self, full_node, solidity_node=None, private_key=None):
+
         if isinstance(full_node, str):
             full_node = HttpProvider(full_node)
 
@@ -24,7 +37,6 @@ class Tron:
 
         self.full_node = full_node
         self.solidity_node = solidity_node
-        self.event_server = event_server
 
         if private_key:
             self.private_key = private_key
