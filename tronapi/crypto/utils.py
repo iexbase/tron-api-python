c652a1c222c7bd5af6e4d797dea169bfbed1fe84
serderovsh <steein.shamsudin@gmail.com>
2018-10-28 12:40:09 +0300

    update utils.py

--- tronapi/crypto/utils.py
+++ tronapi/crypto/utils.py
@@ -1,26 +1,7 @@
-import sys
 
-
-def compat_bytes(item, encoding=None):
-    """Этот метод требуется, потому что Python 2.7 `bytes`
-    является просто псевдонимом для` str`. Без этого метода,
-
-    :param item:
-    :param encoding:
-    :return:
-    """
-    if hasattr(item, '__bytes__'):
-        return item.__bytes__()
-    else:
-        if encoding:
-            return bytes(item, encoding)
-        else:
-            return bytes(item)
-
-
-def is_numeric(x):
+def is_numeric(x) -> bool:
     return isinstance(x, int)
 
 
-def is_string(x):
+def is_string(x) -> bool:
     return isinstance(x, bytes)
