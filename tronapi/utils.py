be4468fd32d69cb796436389062f404fe381a9f4
serderovsh <steein.shamsudin@gmail.com>
2018-10-28 16:47:18 +0300

    fix type

--- tronapi/utils.py
+++ tronapi/utils.py
@@ -1,7 +1,14 @@
+import string
+
 
 def is_numeric(x) -> bool:
     return isinstance(x, int)
 
 
 def is_string(x) -> bool:
-    return isinstance(x, bytes)
+    return isinstance(x, str)
+
+
+def is_hex(s):
+    hex_digits = set(string.hexdigits)
+    return all(c in hex_digits for c in s)
