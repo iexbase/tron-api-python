a6dfdafdef653de8992824371ab0b3839c427641
serderovsh <steein.shamsudin@gmail.com>
2018-11-10 09:24:02 +0300

    update example (issue #12, #13)

--- examples/send-transaction.py
+++ examples/send-transaction.py
@@ -1,25 +1,20 @@
-import json
-
 from tronapi import Tron
 from tronapi import HttpProvider
 
 full_node = HttpProvider('https://api.trongrid.io')
 solidity_node = HttpProvider('https://api.trongrid.io')
 event_server = HttpProvider('https://api.trongrid.io')
+
 tron = Tron(full_node,
             solidity_node,
             event_server)
+
 tron.private_key = 'private_key'
 tron.default_address = 'default address'
 
-result = tron.trx.send('ToAddress', 1)
-
-# Получаем результат
-print(result)
-
-
 # added message
-
 send = tron.trx.send_transaction('to', 1, {
     'message': 'hello'
 })
+
+print(send)
