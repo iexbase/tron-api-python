93249642179f16d0b65521ba13911d0efb9971eb
serderovsh <steein.shamsudin@gmail.com>
2018-11-11 08:18:29 +0300

    update status

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     zip_safe=False,
     python_requires='>=3.5.3,<4',
     classifiers=[
-        'Development Status :: 3 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
