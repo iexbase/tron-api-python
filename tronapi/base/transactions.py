146229b2a3c497f8e493f014b7a3beb711917018
serderovsh <steein.shamsudin@gmail.com>
2019-01-08 14:00:22 +0300

    wait for transaction receipt does not wait for ref_block_hash to exist

--- tronapi/base/transactions.py
+++ tronapi/base/transactions.py
@@ -5,7 +5,8 @@ def wait_for_transaction_id(tron, tx_id, timeout=120, poll_latency=0.1):
     with Timeout(timeout) as _timeout:
         while True:
             tx_detail = tron.trx.get_transaction(tx_id)
-            if tx_detail is not None:
+            # FIXME: The check for a null `ref_block_hash` is due to parity's
+            if tx_detail is not None and tx_detail['ref_block_hash'] is not None:
                 break
             _timeout.sleep(poll_latency)
     return tx_detail
