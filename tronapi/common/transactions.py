f1239a86806bb2f25381c715028ef308d349d2c7
serderovsh <steein.shamsudin@gmail.com>
2019-01-24 21:53:17 +0300

    fix wait_for_transaction_id

--- tronapi/common/transactions.py
+++ tronapi/common/transactions.py
@@ -6,7 +6,9 @@ def wait_for_transaction_id(tron, tx_id, timeout=120, poll_latency=0.1):
         while True:
             tx_detail = tron.trx.get_transaction(tx_id)
             # FIXME: The check for a null `ref_block_hash` is due to parity's
-            if tx_detail is not None and tx_detail['ref_block_hash'] is not None:
+            if tx_detail is not None and \
+                    'raw_data' in tx_detail and \
+                    tx_detail['raw_data']['ref_block_hash'] is not None:
                 break
             _timeout.sleep(poll_latency)
     return tx_detail
